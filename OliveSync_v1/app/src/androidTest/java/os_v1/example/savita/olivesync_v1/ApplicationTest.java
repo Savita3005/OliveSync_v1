98774515ba6ec3c5caa45990164fdec816829c8f
Savita3005 <savita.panigrahi@ob10it.com>
2015-09-01 14:32:41 +0530

    added Version1 of OliveSync project

---
+++ OliveSync_v1/app/src/androidTest/java/os_v1/example/savita/olivesync_v1/ApplicationTest.java
@@ -0,0 +1,13 @@
+package os_v1.example.savita.olivesync_v1;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
